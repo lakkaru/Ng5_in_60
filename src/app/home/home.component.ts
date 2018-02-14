b8ecad6c2b5424be308366ef7128d841da827d12
Lakshman Karunasena <lakkaru@gmail.com>
2018-02-14 05:41:46 +0530

    Event binding created

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,13 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  itemCount:number=4;
+  // itemCount:number=4;
+  itemCount:number;
   btnText:string='Add New Item';
   goalText:string="My life first goal";
-
+  golas=[];
   constructor() { }
 
   ngOnInit() {
+    this.itemCount=this.golas.length;
+  }
+
+  addItem(){
+    this.golas.push(this.goalText);
+    this.goalText="";
+    this.itemCount=this.golas.length;
   }
 
 }
